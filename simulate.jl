a83308cf033ab912a55b3f984676a0a9009f8ec1
DavidGarHeredia <dgheredia@protonmail.com>
2022-11-10 20:12:39 +0100

    feat: make simulate.jl type stable

--- simulate.jl
+++ simulate.jl
@@ -7,7 +7,6 @@ using Base.Threads
 
 include("initialize_state.jl")
 include("hamiltonians.jl")
-include("time_evolution.jl")
 include("measure.jl")
 
 
@@ -26,32 +25,61 @@ function simulate(;
     T::Float64,
     entanglement::Bool
 )
-    spin_levels = [0, 1]  # 0 = +, 1 = -
-    # Basis of states of the camera
-    qubits_states = repeat([spin_levels], nqubits)
-    c_basis = map(sort, map(collect, product(qubits_states...)))
-    c_basis = reshape(c_basis, length(c_basis))
+    c_basis = create_basis_of_states_of_the_camera(nqubits)
     projectors = [diagm(row) for row in eachrow((1*I)(2^nqubits))]
-    # Initialize the global state
-    ρ_0 = initialize_state(nqubits, γ_s, B, T, entanglement)
-    # Generate combinations of D, theta and d_s and simulate
-    measures = Vector{Vector{Vector{Int64}}}(undef, nconfigs) #, nmeasures, nqubits)
-    D_vals = rand(Uniform(D[:low], D[:high]), nconfigs)
-    θ_vals = rand(Uniform(θ[:low], θ[:high]), nconfigs)
-    d_s_vals = rand(Uniform(d_s[:low], d_s[:high]), nconfigs)
+    D_vals, θ_vals, d_s_vals = generate_params_combinations_for_the_simulation(D, θ, d_s, nconfigs)
+
+    S, S_a, S_b = build_extended_spin_operators(nqubits)
+    H_c = build_camera_hamiltonian(S, γ_c, B)
+    H_s = build_system_hamiltonian(S_a, S_b, γ_s, B)
+    ρ_0 = initialize_global_state(nqubits, γ_s, B, T, entanglement)
+
     p = Progress(nconfigs) # Progress bar
-    @threads for i in 1:nconfigs
-        # Build the hamiltonian
-        H = get_H(nqubits, d_c, d_s_vals[i], D_vals[i], θ_vals[i], γ_s, γ_c, B)
-        # Time evolution
-        U = get_U(H, t)
-        ρ_t = time_evolution(ρ_0, U)
-        # Measure
+    measures = Vector{Vector{Vector{Int64}}}(undef, nconfigs) #, nmeasures, nqubits)
+    for i in 1:nconfigs
+        H = build_hamiltonian(H_c, H_s, S, S_a, S_b, d_c, d_s_vals[i], D_vals[i], θ_vals[i], γ_s, γ_c)
+        ρ_t = time_evolution(ρ_0, H, t)
         probabilities = get_probabilities(ρ_t, projectors)
-        measures[i] = measure(c_basis, probabilities, nmeasures) # @inbounds 
-        # Update progress bar
-        next!(p)
+        measures[i] = measure(c_basis, probabilities, nmeasures) # @inbounds
+        next!(p)  # Update progress bar
     end
 
     return measures
+end
+
+
+function time_evolution(ρ_i::Matrix{ComplexF64}, H::Matrix{ComplexF64}, t::Float64)
+    ħ = 1.0545718176461565e-34 # J s
+    U = exp(-1im*H*t/ħ)
+    ρ_f = U*ρ_i*U'
+    return ρ_f
+end
+
+function build_extended_spin_operators(nqubits::Int)
+    S = create_big_spin_operator(nqubits)
+    S_a = extended_pauli_operators(dim_left=2^0, dim_right=2^(nqubits+1))
+    S_b = extended_pauli_operators(dim_left=2^1, dim_right=2^nqubits)
+    return S, S_a, S_b
+end
+
+function generate_params_combinations_for_the_simulation(
+    D::Dict{Symbol, Any},
+    θ::Dict{Symbol, Any},
+    d_s::Dict{Symbol, Any},
+    nconfigs::Int,
+)
+    D_vals = rand(Uniform(D[:low], D[:high]), nconfigs)
+    θ_vals = rand(Uniform(θ[:low], θ[:high]), nconfigs)
+    d_s_vals = rand(Uniform(d_s[:low], d_s[:high]), nconfigs)
+    return D_vals, θ_vals, d_s_vals
+end
+
+function create_basis_of_states_of_the_camera(nqubits::Int)::Vector{Vector{Int64}}
+    spin_levels = [0, 1]  # 0 = +, 1 = -
+    qubits_states = repeat([spin_levels], nqubits)
+    c_basis = [Vector{Int}()]
+    for state in qubits_states
+        c_basis = [vcat(x, y) for x in c_basis for y in state]
+    end
+    return c_basis
 end
